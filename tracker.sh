876a1c8bdc8e27c9c8da511938efb1a2d34cade3
Keeley Hoek <escortkeel@gmail.com>
2014-05-23 19:32:41 +1000

    compatibility

--- tracker.sh
+++ tracker.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
-PRETTIFY=/home/junkbot/problem_tracker/prettify.py
-STUDENTS=/home/junkbot/problem_tracker/STUDENTS
-PROBLEMS=/home/junkbot/problem_tracker/PROBLEMS
-TEMPLATE=/home/junkbot/problem_tracker/tracker_template.html
-OUTPUT=/home/junkbot/public_html/tracker/index.html
+# Get the directory of this script
+# see http://stackoverflow.com/questions/59895
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+PRETTIFY=$DIR/prettify.py
+STUDENTS=$DIR/STUDENTS
+PROBLEMS=$DIR/PROBLEMS
+TEMPLATE=$DIR/tracker_template.html
+OUTPUT=$HOME/public_html/tracker/index.html
 
 $PRETTIFY $STUDENTS $PROBLEMS $TEMPLATE > $OUTPUT
